900c232526c5723dc0952062c3b2eb325e05b690
AllenMaz <2727954462@qq.com>
2017-04-13 22:50:56 +0800

    类转换

---
+++ PublicModel/ClassConvert.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace PublicModel
+{
+    /// <summary>
+    /// 类转换
+    /// </summary>
+    public class ClassConvert
+    {
+
+        public static IList<TargetT> Convert<SourceT, TargetT>(IList<SourceT> sources) where TargetT : new()
+        {
+            IList<TargetT> targets = new List<TargetT>();
+            foreach (SourceT source in sources)
+            {
+                TargetT target = new TargetT();
+                target = Convert<SourceT, TargetT>(source);
+                targets.Add(target);
+            }
+
+            return targets;
+        }
+
+        public static TargetT Convert<SourceT, TargetT>(SourceT source)
+        {
+            
+
+            Type typeSource = typeof(SourceT);
+            Type typeTarget = typeof(TargetT);
+
+            //动态创建目标实例
+            dynamic targetInstance = typeTarget.Assembly.CreateInstance(typeTarget.ToString()); 
+
+            foreach (PropertyInfo sProperty in typeSource.GetProperties())
+            {
+                try
+                {
+                    // 属性名
+                    var sPropertyName = sProperty.Name;
+                    var sPropertyTypeName = sProperty.PropertyType.ToString();
+
+                    //获取目标实例的属性
+                    PropertyInfo tProperty = typeTarget.GetProperty(sPropertyName);
+                    if (tProperty != null && tProperty.CanWrite) 
+                    {
+                        var tPropertyTypeName = tProperty.PropertyType.ToString();
+                        object sValue = typeSource.GetProperty(sPropertyName).GetValue(source, null);
+
+                        if (sValue != null && !String.IsNullOrEmpty(sValue.ToString()))
+                        {
+                            if(sPropertyTypeName == tPropertyTypeName){
+                                //如果类型相同，则直接转换
+                                tProperty.SetValue(targetInstance,sValue,null);
+                            }
+                            else
+                            {
+                                var convertValue = sValue;
+                                switch (tPropertyTypeName)
+                                {
+                                    case "System.DateTime":
+                                        convertValue = DateTime.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.DateTime]":
+                                        convertValue = DateTime.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Int32":
+                                        convertValue = Int32.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Int64":
+                                        convertValue = Int64.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Int32]":
+                                        convertValue = Int32.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Int64]":
+                                        convertValue = Int64.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Double":
+                                        convertValue = Double.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Double]":
+                                        convertValue = Double.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Decimal":
+                                        convertValue = Decimal.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Decimal]":
+                                        convertValue = Decimal.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Single]":
+                                        convertValue = Single.Parse(sValue.ToString());
+
+                                        break;
+                                    case "System.Single":
+                                        convertValue = Single.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Boolean":
+                                        convertValue = Boolean.Parse(sValue.ToString());
+                                        break;
+                                    case "System.Nullable`1[System.Boolean]":
+                                        convertValue = int.Parse(sValue.ToString());
+                                        break;
+                                    case "System.String":
+                                        convertValue = sValue.ToString();
+                                        break;
+                                    case "System.Byte[]":
+                                        convertValue = sValue;
+                                        break;
+                                    case "System.Collections.Generic.List`1":
+
+                                        break;
+                                    case "System.Collections.Generic.ICollection`1":
+                                    default:
+                                        //集合判断
+                                        if (sProperty.PropertyType.IsGenericType && tProperty.PropertyType.IsGenericType)
+                                        {
+                                            //获取实际类型
+                                            var sActType = sProperty.PropertyType.GetGenericArguments().First();
+                                            var tActType = tProperty.PropertyType.GetGenericArguments().First();
+
+                                            //获取IList<TargetT> Convert<SourceT, TargetT>(IList<SourceT> sources)方法
+                                            //泛型调用方法进行集合转换
+                                            MethodInfo method = typeof(ClassConvert).GetMethods()
+                                             .First(v=>v.Name =="Convert" && v.IsGenericMethod)
+                                            .MakeGenericMethod(new Type[] {sActType,tActType });
+
+                                            convertValue = method.Invoke(null, new object[] { sValue });
+                                        }
+                                        
+                                        //其它情况不做任何操作
+                                        break;
+                                }
+                                tProperty.SetValue(targetInstance, convertValue, null);
+                            }
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(sProperty.Name + ex.Message);
+                }
+            }
+
+            return targetInstance;
+        }
+
+
+    
+    }
+}
---
+++ UnitTest/ClassConvertTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PublicUtility;
+using PublicModel;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ClassConvertTests
+    {
+        [TestMethod()]
+        public void ConvertTest()
+        {
+
+            ModelClass model = new ModelClass()
+            {
+                id = "234234234sdfsdfsdfsdfsdfsdf",
+                number = "23",
+                Name = "allen",
+                IsRight = "true",
+                IsWrong = "false",
+                Date = "2017-09-09",
+                CreateDate = "2017-12-12",
+                childs = new List<ModelClassChild>() { 
+                   new ModelClassChild(){
+                    id= "1111",
+                    childname ="child1"
+                   },
+                    new ModelClassChild(){
+                    id= "2222",
+                    childname ="child2"
+                   }
+                }
+            };
+
+            var convertSuccess = true;
+            try
+            {
+                EntityClass entity = new EntityClass();
+                entity = ClassConvert.Convert<ModelClass, EntityClass>(model);
+
+
+                ModelClass cM = ClassConvert.Convert<EntityClass, ModelClass>(entity);
+
+                var aa = true;
+            }
+            catch(Exception e)
+            {
+                convertSuccess = false;
+            }
+
+
+            Assert.IsTrue(convertSuccess);
+        }
+    }
+
+
+    public class ModelClass:BaseModel
+    {
+        public string id { get; set; }
+
+        public string number { get; set; }
+
+        public string Name { get; set; }
+
+        public string IsRight { get; set; }
+
+        public string IsWrong { get; set; }
+
+        public string Date { get; set; }
+
+        public string CreateDate { get; set; }
+
+        public List<ModelClassChild> childs { get; set; }
+
+        public ModelClass()
+        {
+            childs = new List<ModelClassChild>();
+        }
+
+    }
+
+    public class ModelClassChild
+    {
+
+        public string id { get; set; }
+
+        public string childname { get; set; }
+
+       
+
+    }
+
+    public class EntityClass
+    {
+        public string id { get; set; }
+
+        public Int32 number { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsRight { get; set; }
+
+        public bool IsWrong { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public List<EntityClassChild> childs { get; set; }
+
+        public EntityClass()
+        {
+            childs = new List<EntityClassChild>();
+        }
+    }
+
+    public class EntityClassChild
+    {
+
+        public int id { get; set; }
+
+        public string childname { get; set; }
+    }
+}
